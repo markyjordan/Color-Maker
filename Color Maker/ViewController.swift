66592d607eae7701d3b21a232d8aad9e010bd566
Mark Jordan <markybjordan@gmail.com>
2020-03-22 19:57:49 -0700

    Created new project and added the default template files to the repo.

---
+++ Color Maker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Color Maker
+//
+//  Created by Marky Jordan on 3/22/20.
+//  Copyright © 2020 Marky Jordan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
